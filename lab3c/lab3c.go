26913dbfc09119ccea653c0b5b08e9b7f5f3b37b
Afohtim <afohmouth@gmail.com>
2020-02-12 14:05:51 +0200

    channels are not global now

--- lab3c/lab3c.go
+++ lab3c/lab3c.go
@@ -7,10 +7,6 @@ import (
 )
 
 type semaphore chan bool
-
-var currentItemSemaphore = make(semaphore, 1)
-var found = make(semaphore, 1)
-var finish_work = make(chan int)
 var current_item_id = -1
 
 var item_names = map[int]string{
@@ -19,7 +15,7 @@ var item_names = map[int]string{
 	3: "matches",
 }
 
-func smoker(missing_item_id int) {
+func smoker(missing_item_id int, currentItemSemaphore chan bool, found chan bool) {
 	for true {
 		currentItemSemaphore <- true
 
@@ -33,7 +29,7 @@ func smoker(missing_item_id int) {
 	}
 }
 
-func intermediator(turns_count int) {
+func intermediator(turns_count int, currentItemSemaphore chan bool, found chan bool, finish_work chan int) {
 	for i := 0; i < turns_count; i++ {
 		currentItemSemaphore <- true
 		current_item_id = rand.Intn(3) + 1
@@ -47,11 +43,15 @@ func intermediator(turns_count int) {
 }
 
 func main() {
-	go smoker(1)
-	go smoker(2)
-	go smoker(3)
 
-	go intermediator(15)
+	var currentItemSemaphore = make(semaphore, 1)
+	var found = make(semaphore, 1)
+	var finish_work = make(chan int)
+	go smoker(1, currentItemSemaphore, found)
+	go smoker(2, currentItemSemaphore, found)
+	go smoker(3, currentItemSemaphore, found)
+
+	go intermediator(15, currentItemSemaphore, found, finish_work)
 
 	<-finish_work
 }
